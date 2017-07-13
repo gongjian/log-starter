29bba319849528d51ab73cdbd5445cbc917a348d
gongjian <gongjian@172.20.94.2>
2017-07-13 14:38:26 +0800

    Initial Version

---
+++ src/main/java/com/gj/test/autoconfigure/LogAutoConfiguration.java
@@ -0,0 +1,50 @@
+package com.gj.test.autoconfigure;
+
+import javax.annotation.PostConstruct;
+
+import org.aopalliance.aop.Advice;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.aop.Pointcut;
+import org.springframework.aop.support.AbstractPointcutAdvisor;
+import org.springframework.aop.support.annotation.AnnotationMatchingPointcut;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import com.gj.test.annotation.Log;
+import com.gj.test.aop.LogMethodInterceptor;
+
+@Configuration
+@EnableConfigurationProperties(LogProperties.class)
+public class LogAutoConfiguration extends AbstractPointcutAdvisor {
+
+	private static final long serialVersionUID = 1L;
+
+	private Logger logger = LoggerFactory.getLogger(LogAutoConfiguration.class);
+
+	private Pointcut pointcut;
+	private Advice advice;
+
+	@Autowired
+	private LogProperties logProperties;
+
+	@PostConstruct
+	public void init() {
+		logger.info("init LogAutoConfiguration start");
+		this.pointcut = new AnnotationMatchingPointcut(null, Log.class);
+		this.advice = new LogMethodInterceptor(logProperties.getExcludeArr());
+		logger.info("init LogAutoConfiguration end");
+	}
+
+	@Override
+	public Pointcut getPointcut() {
+		return this.pointcut;
+	}
+
+	@Override
+	public Advice getAdvice() {
+		return this.advice;
+	}
+
+}
